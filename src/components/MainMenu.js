f653306e940a0e89bfbabbb0705e3e09b3891176
Danny Wilkins <danny.wilkins@dootrix.com>
2019-12-07 10:00:59 +0000

    Add rough menu

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -1,11 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  NavbarText,
+} from 'reactstrap';
+import './mainMenu.scss';
 
 const MainMenu = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [isOpen, setIsOpen] = useState(false);
 
-export default MainMenu
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <Navbar color="primary" light expand="md">
+      <NavbarBrand href="/"><h1>Tellit</h1></NavbarBrand>
+      <NavbarToggler onClick={toggle} />
+      <Collapse isOpen={isOpen} navbar>
+        <Nav className="mr-auto" navbar>
+          <NavItem>
+            <NavLink href="/components/"><h3>Select Universe</h3></NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="https://github.com/reactstrap/reactstrap">
+              <h3>Characters</h3>
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="https://github.com/reactstrap/reactstrap">
+              <h3>Books</h3>
+            </NavLink>
+          </NavItem>
+        </Nav>
+        <NavbarText>Simple Text</NavbarText>
+      </Collapse>
+    </Navbar>
+  );
+};
+
+export default MainMenu;
